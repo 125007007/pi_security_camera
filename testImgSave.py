db31325824d2b7e783531ee19925e7c0bac714d2
125007007 <ashezzone@gmail.com>
2022-02-04 01:05:25 +1300

    Create tesImgSave.py

---
+++ testImgSave.py
@@ -0,0 +1,44 @@
+#Import necessary libraries
+import os, datetime, cv2, sys
+# import custom modules
+from fileManagement import FileManager
+
+
+def dt_file_name():
+    #global file_name
+    # sets file name to current date and time to the nearest second
+    file_name = str(datetime.datetime.now().time()) # date and time with microseconds
+    print(file_name)
+    file_name = list(file_name)
+    file_name[2] = '_'
+    file_name[5] = '_'
+    file_name[8] = '_'
+    file_name = ''.join(file_name)
+    print(len(file_name))
+    return file_name
+    
+
+FileManager().createSnapshotsDir()
+FileManager().createCurrentDateDir()
+FileManager().removeOldDir()
+cap = cv2.VideoCapture(0)
+# set to True if camera is upside down
+#cam_upside_down = True
+#cap.set(3, 1280)
+#cap.set(4, 720)
+
+
+sucess, frame1 = cap.read()
+
+img_name =("snapshot-"+str(dt_file_name())+str(".png"))
+#print(save_dir)
+#cv2.imwrite(FileManager().currentDateDir + '/{}'.format(img_name), frame2)
+cv2.imwrite(os.path.join(FileManager().currentDateDir, img_name), frame1)
+    
+
+# Close down the video stream
+cap.release()
+cv2.destroyAllWindows()
+sys.exit()
+
+

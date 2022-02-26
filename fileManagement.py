02183e3d399cfa09f43c6dfef90fff00be80eb26
125007007 <ashezzone@gmail.com>
2022-02-27 00:01:35 +1300

    Update fileManagement.py

--- fileManagement.py
+++ fileManagement.py
@@ -4,6 +4,9 @@ from logger import SetupLogger
 
 class FileManager(object):
 
+# Info Logger
+    log = SetupLogger.setup_logger('Logger', os.path.join(os.getcwd(), 'fileManagment.log'))
+
     def __init__(self):
         self.todays_date = datetime.date.today()
         self.f = open("config.json")
@@ -12,8 +15,6 @@ class FileManager(object):
             self.pwd = self.data["usb_drive_location"]
         elif self.data["use_usb_drive"] is False:
             self.pwd = os.getcwd()
-        # Info Logger
-        self.log = SetupLogger.setup_logger('Logger', os.path.join(os.getcwd(), 'fileManagment.log'))
         self.new_dir_name = str(self.todays_date)
         # sets the date four days ago
         self.four_days_ago = self.todays_date - datetime.timedelta(days=3)
@@ -25,17 +26,17 @@ class FileManager(object):
 
     def createSnapshotsDir(self):
         if not os.path.exists(self.fullpath):
-            self.log.info("Creating Snapshots Directory")
+            FileManager.log.info("Creating Snapshots Directory")
             os.mkdir(self.fullpath)
 
     def createCurrentDateDir(self):
         if not os.path.exists(self.currentDateDir):
-            self.log.info(f"Creating {self.new_dir_name} Directory")
+            FileManager.log.info(f"Creating {self.new_dir_name} Directory")
             os.mkdir(self.currentDateDir)
 
     def removeOldDir(self):
         # checks if a folder with the name of the date four days ago exists
         if os.path.exists(self.path_to_old_dir) is True:
-            self.log.info(f"Removing {self.old_dir_name} Directory")
+            FileManager.log.info(f"Removing {self.old_dir_name} Directory")
             # remove old dir
             shutil.rmtree(self.path_to_old_dir)

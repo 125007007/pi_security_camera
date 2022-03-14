2e6de3a4a84afdafdf52875ce728c2cf98a6fa10
125007007 <ashezzone@gmail.com>
2022-03-15 01:27:42 +1300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# Install Dependencies
+sudo apt-get install libcblas-dev libhdf5-dev libhdf5-serial-dev libatlas-base-dev
+sudo apt-get install libjasper-dev libqtgui4 libqt4-test libilmbase-dev 
+sudo apt-get install libopenexr-dev libgstreamer1.0-dev libavcodec-dev libavformat-dev
+sudo apt-get install libswscale-dev libwebp-dev
 
+sudo pip3 install flask
+sudo pip3 install numpy
+sudo pip3 install opencv-python
 
 pip3 install flask
 pip3 install numpy
 pip3 install opencv-python
+
+git clone https://github.com/125007007/pi_security_camera.git
+
+cd pi_security_camera
+
+ls

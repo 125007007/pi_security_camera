3188d8f4dd51cacce53c23fae3b95377ab5f8ca5
125007007 <ashezzone@gmail.com>
2022-03-15 01:34:02 +1300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 # Install Dependencies
-sudo apt-get install libcblas-dev libhdf5-dev libhdf5-serial-dev libatlas-base-dev -y
-sudo apt-get install libjasper-dev libqtgui4 libqt4-test libilmbase-dev -y
-sudo apt-get install libopenexr-dev libgstreamer1.0-dev libavcodec-dev libavformat-dev -y
-sudo apt-get install libswscale-dev libwebp-dev -y
+sudo apt-get install libcblas-dev -y
+sudo apt-get install libjasper-dev -y
+sudo apt-get install libopenexr-dev -y
+sudo apt-get install libswscale-dev -y
+sudo apt-get install libhdf5-dev -y
+sudo apt-get install libhdf5-serial-dev -y
+sudo apt-get install libatlas-base-dev -y
+sudo apt-get install libqtgui4 -y
+sudo apt-get install libqt4-test -y
+sudo apt-get install libilmbase-dev -y
+sudo apt-get install libgstreamer1.0-dev -y
+sudo apt-get install libavcodec-dev -y
+sudo apt-get install libavformat-dev -y
+sudo apt-get install libwebp-dev -y
 
-sudo pip3 install flask
-sudo pip3 install numpy
-sudo pip3 install opencv-python
+sudo pip3 install flask -y
+sudo pip3 install numpy -y
+sudo pip3 install opencv-python -y
 
 pip3 install flask
 pip3 install numpy
